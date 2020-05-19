529a568c46b17d02c37a5dfbf6673d1dc8091f47
csosa27 <eng.csosa.92@gmail.com>
2020-05-19 17:53:44 -0400

    Chore: moving ShopShop to TheShopShop

---
+++ README.md
@@ -0,0 +1,2 @@
+### Shop Shop
+#### The Shopping App
---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,142 @@
+import React, { useEffect, useState, useCallback } from 'react';
+import { View, Text, Button, FlatList, Platform, ActivityIndicator, StyleSheet} from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+
+import ProductItem from '../../components/shop/ProductItem';
+import HeaderButton from '../../components/UI/HeaderButton';
+import * as cartActions from '../../store/actions/cart';
+import * as productActions from '../../store/actions/products';
+
+import Colors from '../../constants/Colors';
+
+const ProductOverviewScreen = props => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [isRefreshing, setIsRefreshing] = useState(false);
+    const [error, setError] = useState();
+    const products = useSelector(state => state.products.availableProducts);
+    const dispatch = useDispatch();
+
+    const loadProducts = useCallback(async () => {
+        setError(null);
+        setIsRefreshing(true);
+        try {
+            await dispatch(productActions.fetchProducts());
+        } catch(err) {
+            setError(err.message);
+        }
+        setIsRefreshing(false);
+    }, [dispatch, setIsRefreshing, setError]);
+
+    useEffect(() => {
+        const willFocusSub = props.navigation.addListener('willFocus', loadProducts);
+        return () => {
+            willFocusSub.remove();
+        };
+    }, [loadProducts]);
+
+    useEffect(() => {
+        setIsLoading(true);
+        loadProducts().then(() => {
+            setIsLoading(false);
+        });
+    }, [dispatch, loadProducts]);
+
+    const selectItemHandler = (id, title) => {
+        props.navigation.navigate('ProductDetail', { 
+            productId: id,
+            productTitle: title
+        });
+    }
+
+    if(error){
+        return (
+            <View style={styles.centered}>
+                <Text>An error ocurred!</Text>
+                <Button title='Try Again' onPress={loadProducts} color={Colors.primary} />
+            </View>
+        );
+    }
+
+    if(isLoading) {
+        return (
+            <View style={styles.centered}>
+                <ActivityIndicator size='large' color={Colors.primary}/>
+            </View>
+        );
+    }
+    if(!isLoading && products.length === 0){
+        return (
+            <View style={styles.centered}>
+                <Text>No products found. Maybe start adding some.</Text>
+            </View>
+        );
+    }
+
+    return (
+        <FlatList 
+            onRefresh={loadProducts}
+            refreshing={isRefreshing}
+            data={products} 
+            renderItem={itemData =>
+                <ProductItem 
+                    image={itemData.item.imageUrl}
+                    title={itemData.item.title}
+                    price={itemData.item.price}
+                    onSelect={() => selectItemHandler(itemData.item.id, itemData.item.title)}
+                >
+                    <Button 
+                        color={Colors.primary} 
+                        title="View Details" 
+                        onPress={() => selectItemHandler(itemData.item.id, itemData.item.title)}
+                    />
+                    <Button 
+                        color={Colors.primary} 
+                        title="To Cart" 
+                        onPress={() => {
+                            dispatch(cartActions.addToCart(itemData.item));
+                        }}
+                    />
+                </ProductItem>
+            }
+        />
+    );
+};
+
+ProductOverviewScreen.navigationOptions = navData => {
+    return {
+        headerTitle: 'All Products',
+        headerLeft: () => (
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item 
+                    title='Menu' 
+                    iconName={Platform.OS ==='android' ? 'md-menu' : 'ios-menu'} 
+                    onPress={() =>{
+                        navData.navigation.toggleDrawer();
+                    }}
+                />
+            </HeaderButtons>
+        ),
+        headerRight: () => (
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item 
+                    title='Cart' 
+                    iconName={Platform.OS ==='android' ? 'md-cart' : 'ios-cart'} 
+                    onPress={() =>{
+                        navData.navigation.navigate('Cart')
+                    }}
+                />
+            </HeaderButtons>
+        )
+    };
+};
+
+const styles = StyleSheet.create({
+    centered: {
+        flex: 1, 
+        justifyContent: 'center', 
+        alignItems: 'center'
+    }
+})
+
+export default ProductOverviewScreen;
